db950c4196d4d244c0ed0f6043d29cebc2b6bc99
ZhMike <zh-mike@mail.ru>
2022-09-02 12:19:07 +0500

    Доработал текст, убрал ошибку когда вводишь какую нибудь чушь!

--- SuEFA/Program.cs
+++ SuEFA/Program.cs
@@ -1,7 +1,7 @@
 ﻿while (true)
 {
     Console.WriteLine("Игра камень, ножницы, бумага!!!");
-    Console.Write("Введите свой вариант: ");
+    Console.WriteLine("Введите свой вариант. (Для выхода введите 'q'):");
     string userAnswer = Console.ReadLine();
     string[] variants = { "ножницы", "бумага", "камень" };
     int i = new Random().Next(0, 3);
@@ -11,6 +11,11 @@
     if (userAnswer == "q") break;
 
     Console.WriteLine(userAnswer + " vs " + computeAnswer);
+
+    if (userAnswer != variants[0] & userAnswer != variants[1] & userAnswer != variants[2])
+    {
+        Console.WriteLine("Дурень смотри что пишешь!!!");
+    }
 
     if (userAnswer == computeAnswer) Console.WriteLine("Ничья!");
 

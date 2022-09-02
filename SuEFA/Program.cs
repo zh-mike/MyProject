1bfb9f10f4809de4d685ec3da67c1c27acf5a3c1
ZhMike <zh-mike@mail.ru>
2022-09-02 11:51:30 +0500

    Добавил цикл и реализовал выход!

--- SuEFA/Program.cs
+++ SuEFA/Program.cs
@@ -1,25 +1,32 @@
-﻿Console.WriteLine("Игра камень, ножницы, бумага!!!");
-Console.Write("Введите свой вариант: ");
-string userAnswer = Console.ReadLine();
-string[] variants = { "ножницы", "бумага", "камень"};
-int i = new Random().Next(0, 3);
-string computeAnswer = variants[i];
-userAnswer = userAnswer.ToLower();
-Console.WriteLine(userAnswer + " vs " + computeAnswer);
-
-if (userAnswer == computeAnswer) Console.WriteLine("Ничья!");
-
-void comparison(int a, int b, int c)
+﻿while (true)
 {
-    if (userAnswer == variants[a] & computeAnswer == variants[b]) 
+    Console.WriteLine("Игра камень, ножницы, бумага!!!");
+    Console.Write("Введите свой вариант: ");
+    string userAnswer = Console.ReadLine();
+    string[] variants = { "ножницы", "бумага", "камень" };
+    int i = new Random().Next(0, 3);
+    string computeAnswer = variants[i];
+    userAnswer = userAnswer.ToLower();
+
+    if (userAnswer == "q") break;
+
+    Console.WriteLine(userAnswer + " vs " + computeAnswer);
+
+    if (userAnswer == computeAnswer) Console.WriteLine("Ничья!");
+
+    void comparison(int a, int b, int c)
     {
-    Console.WriteLine("Поздравляю вы победили!!!");
+        if (userAnswer == variants[a] & computeAnswer == variants[b])
+        {
+            Console.WriteLine("Поздравляю вы победили!!!");
+        }
+        if (userAnswer == variants[a] & computeAnswer == variants[c])
+        {
+            Console.WriteLine("Кажется вас размазали!)");
+        }
     }
-    if (userAnswer == variants[a] & computeAnswer == variants[c]) 
-    {
-    Console.WriteLine("Кажется вас размазали!)");
-    }
+    comparison(0, 1, 2);
+    comparison(1, 2, 0);
+    comparison(2, 0, 1);
+    Console.WriteLine();
 }
-comparison(0, 1, 2);
-comparison(1, 2, 0);
-comparison(2, 0, 1);
